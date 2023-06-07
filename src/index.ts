1fcc074ee5b9f8ec9a48821bc71af3d1d8a00410
ruleeeer <me@ruleeeer.cn>
2023-06-07 22:58:47 +0800

    chore: modify url address

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ function getTodayQuestion() {
         "variables": {},
         "query": "query questionOfToday { todayRecord {   question {     questionFrontendId     questionTitleSlug     __typename   }   lastSubmission {     id     __typename   }   date   userStatus   __typename }}"
     };
-    return fetch('https://leetcode-cn.com/graphql', {
+    return fetch('https://leetcode.cn/graphql', {
         method: 'POST',
         body: JSON.stringify(query),
         headers: {
@@ -36,7 +36,7 @@ function getQuestionByTitleId(titleId) {
                 "query questionData($titleSlug: String!) {  question(titleSlug: $titleSlug) {    questionId    questionFrontendId    boundTopicId    title    titleSlug    content    translatedTitle    translatedContent    isPaidOnly    difficulty    likes    dislikes    isLiked    similarQuestions    contributors {      username      profileUrl      avatarUrl      __typename    }    langToValidPlayground    topicTags {      name      slug      translatedName      __typename    }    companyTagStats    codeSnippets {      lang      langSlug      code      __typename    }    stats    hints    solution {      id      canSeeDetail      __typename    }    status    sampleTestCase    metaData    judgerAvailable    judgeType    mysqlSchemas    enableRunCode    envInfo    book {      id      bookName      pressName      source      shortDescription      fullDescription      bookImgUrl      pressImgUrl      productUrl      __typename    }    isSubscribed    isDailyQuestion    dailyRecordStatus    editorType    ugcQuestionId    style    __typename  }}"
         };
 
-    return fetch('https://leetcode-cn.com/graphql', {
+    return fetch('https://leetcode.cn/graphql', {
         method: 'POST',
         body: JSON.stringify(query),
         headers: {
