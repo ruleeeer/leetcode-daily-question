7cbc1616ce9cda34c36c840a1b07824d4f48150c
ruleeeer <me@ruleeeer.cn>
2023-06-11 15:22:26 +0800

    feat: support esm and cjs format

--- build.js
+++ build.js
@@ -1,11 +1,22 @@
-
 import {build} from "esbuild";
+
 build({
     entryPoints: ['./src/index.ts'],
-    outfile: './dist/index.mjs',
     platform: 'node',
     target: 'node16',
     bundle: true,
-    format:"esm",
-    minify:true
+    format: 'esm',
+    outfile: './dist/index.mjs',
+    minify: true
 }).catch(() => process.exit(1));
+
+build({
+    entryPoints: ['./src/index.ts'],
+    platform: 'node',
+    target: 'node16',
+    bundle: true,
+    format: 'cjs',
+    outfile: './dist/index.cjs',
+    minify: true
+}).catch(() => process.exit(1));
+
